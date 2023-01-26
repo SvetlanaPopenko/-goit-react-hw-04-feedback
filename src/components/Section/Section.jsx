73649d5b00128c24178534cb5532e71be30a643e
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-27 00:31:21 +0200

    Create Section.jsx

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import { SectionTag, Title } from './Section.styled';
+
+const Section = ({ title, children }) => {
+    return (
+        <SectionTag>
+            <Title>{title}</Title>{children}
+        </SectionTag>
+    );
+};
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node,
+};
+
+export default Section;
