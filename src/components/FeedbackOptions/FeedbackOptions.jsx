7ab9cd8b52b9f5b2a7df95c12a89ba2f0dc46fd2
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-27 00:31:01 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,5 +1,23 @@
-import './FeedbackOptions.styled';
+import PropTypes from 'prop-types';
 
-const FeedbackOptions = ({options, onLeaveFeedback})=>{
-    return ();
-}
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul>
+      {options.map((option, idx) => (
+        <li key={idx}>
+          <button onClick={() => onLeaveFeedback(option)}>
+            {option[0].toUpperCase() + option.slice(1, option.length)}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.array.isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
