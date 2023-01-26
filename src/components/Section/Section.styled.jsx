35f570ed499f17079f4308253040e6be635f3cac
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-27 00:31:24 +0200

    Create Section.styled.jsx

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const SectionTag = styled.section`
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  align-items: center;
+   flex-direction: column;
+  width: 500px;
+  gap:20px;
+ 
+`;
+
+export const Title = styled.h2`
+font-size:24px;
+    &:hover {
+    border-bottom: 1px solid black;
+  }
+`;
