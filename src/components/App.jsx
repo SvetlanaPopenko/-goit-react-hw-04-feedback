2db5de84515cd68c73ffcf81d7ca5fcaf72807d1
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-26 21:17:09 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,57 @@
 import {Component} from "react";
 import Statistics from "./Statistics";
+import Proptypes from 'prop-types';
+import FeedbackOptions from "./FeedbackOptions";
+import Statistics from "./Statistics";
 
 
 export class App extends Component {
+    static defaultProps = {
+    state: PropTypes.shape({
+      good: PropTypes.number.isRequired,
+      neutral: PropTypes.number.isRequired,
+      bad: PropTypes.number.isRequired,
+    }).isRequired,
+  };
+
   state = {
     good: 0,
     neutral: 0,
     bad: 0
   };
 
-  countTotalFeedback();
-  countPositiveFeedbackPercentage()
+  onLeaveFeedback = event => {
+    this.setState(prevState => ({ [event]: prevState[event] + 1 }));
+  };
 
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101'
-        }}
-      >
-        <h1>Please leave feedback</h1>
-        <FeedbackOptions options={} onLeaveFeedback={}></FeedbackOptions>
-        <h2>Statistics</h2>
-        <Statistics good={good} neutral={neutral} bad={bad} total={} positivePercentage={}></Statistics>
-      </div>
-    );
-  }
-}
+    countTotalFeedback = () => 
+      Object.values(this.state).reduce((acc, value) => (acc += value), 0);
+    
+  countPositiveFeedbackPercentage = ({ good } = this.state) => {
+    return Math.round((good * 100) / this.countTotalFeedback() || 0);
+    };
+
+    render() {
+      const { good, neutral, bad } = this.state;
+      const options = Object.keys(this.state);
+
+      return (
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101'
+          }}
+        >
+          <h1>Please leave feedback</h1>
+          <FeedbackOptions options={options} onLeaveFeedback={this.onLeaveFeedback}></FeedbackOptions>
+          <h2>Statistics</h2>
+          <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={ }></Statistics>
+        </div>
+      );
+    }
+  };
