4d2d44e133ce36ddea13d6510fef67923ae4b1da
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-28 00:36:34 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,16 +2,17 @@ import PropTypes from 'prop-types';
 import { SectionTag, Title } from './Section.styled';
 
 const Section = ({ title, children }) => {
-    return (
-        <SectionTag>
-            <Title>{title}</Title>{children}
-        </SectionTag>
-    );
+  return (
+    <SectionTag>
+      <Title>{title}</Title>
+      {children}
+    </SectionTag>
+  );
 };
 
 Section.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.node,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
 };
 
 export default Section;
