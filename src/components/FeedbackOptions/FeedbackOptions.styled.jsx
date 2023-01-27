0e8e51c2d32082013c70d075c907f1f723e86b55
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-27 19:09:40 +0200

    Update FeedbackOptions.styled.jsx

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,2 +1,26 @@
+import styled from '@emotion/styled';
 
+export const FeedbackList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  list-style: none;
+`;
 
+export const FeedbackItem = styled.li``;
+
+export const FeedbackButton = styled.button`
+  font-size: 24px;
+  color: white;
+  border-radius: 23%;
+  background: linear-gradient(160deg, #ed8bf8, #6d0462);
+  padding: 10px;
+  cursor: pointer;
+  :hover {
+    transform: scale(1.05);
+    font-size: 28px;
+    color: #f8f856;
+    box-shadow: 1px 3px 2px blanchedalmond, -1px -3px 5px #ed8bf8;
+  }
+`;
