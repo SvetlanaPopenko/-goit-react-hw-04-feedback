c163e93e4b358e19584340b1c1649ebdec93b324
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-28 00:36:41 +0200

    Update Statistics.styled.jsx

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,21 +1,22 @@
 import styled from '@emotion/styled';
 
 export const StatisticList = styled.ul`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-    flex-wrap: wrap;
-    list-style: none;
-    border-radius: 15px;
-    margin: 0px auto;
-       padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  flex-wrap: wrap;
+  list-style: none;
+  border-radius: 15px;
+  margin: 0px auto;
+  padding: 10px;
 `;
 
 export const StatisticItem = styled.li`
-font-size: 20px;
-border: 2px solid yellow;
-border-radius: 40%;
-padding: 10px;
-background: linear-gradient(160deg, #f0ade4, #d108ec);
+  font-size: 20px;
+  width: max-content;
+  border: 2px solid yellow;
+  border-radius: 40%;
+  padding: 10px;
+  background: linear-gradient(160deg, #f0ade4, #d108ec);
 `;
