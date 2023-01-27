14e3e4655da4eb675c021c75a1a894cdbf75458d
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-27 23:26:09 +0200

    Update Statistics.styled.jsx

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const StatisticList = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    flex-wrap: wrap;
+    list-style: none;
+    border: 1px solid red;
+    border-radius: 15px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 10px;
+`;
+
+export const StatisticItem = styled.li`
+font-size: 20px;
+`;
