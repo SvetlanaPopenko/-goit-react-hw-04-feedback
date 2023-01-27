2c446562a33f3823df6390a734e29de8ce73db8a
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-27 19:09:47 +0200

    Update Section.styled.jsx

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,19 +1,14 @@
 import styled from '@emotion/styled';
 
 export const SectionTag = styled.section`
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
+    display: flex;
   align-items: center;
-   flex-direction: column;
+  flex-direction: column;
   width: 500px;
-  gap:20px;
- 
-`;
+  gap: 20px;
+  `;
 
 export const Title = styled.h2`
-font-size:24px;
-    &:hover {
-    border-bottom: 1px solid black;
-  }
+  font-size: 24px;
+  color:#a50795;
 `;
