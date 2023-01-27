675e15397608622e51fa09eaee6618edb2241756
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-28 00:19:34 +0200

    Update Section.styled.jsx

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -4,11 +4,12 @@ export const SectionTag = styled.section`
     display: flex;
   align-items: center;
   flex-direction: column;
-  width: 500px;
   gap: 20px;
+  margin: 10px;
   `;
 
 export const Title = styled.h2`
   font-size: 24px;
   color:#a50795;
+  margin: 0;
 `;
