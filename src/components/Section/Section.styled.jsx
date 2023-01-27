0ebe0a2279c598dfb7cc2be3f9919de48f0d4477
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-28 00:36:37 +0200

    Update Section.styled.jsx

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,15 +1,15 @@
 import styled from '@emotion/styled';
 
 export const SectionTag = styled.section`
-    display: flex;
+  display: flex;
   align-items: center;
   flex-direction: column;
   gap: 20px;
-  margin: 10px;
-  `;
+  margin: 0;
+`;
 
 export const Title = styled.h2`
   font-size: 24px;
-  color:#a50795;
+  color: #a50795;
   margin: 0;
 `;
