c2bad636b4b4383a6180b75fb9a8fba818b622ba
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-27 19:09:44 +0200

    Update Notification.styled.jsx

--- src/components/Notification/Notification.styled.jsx
+++ src/components/Notification/Notification.styled.jsx
@@ -3,6 +3,6 @@ import styled from '@emotion/styled';
 export const Text = styled.p`
   margin: 0;
   font-weight: 500;
-  font-size: 16px;
-  border-bottom: 1px solid black;
-`;
+  font-size: 20px;
+  color: #a50795;
+   `;
