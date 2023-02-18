4f175359a9ee800cb581070ccaa7318879c75b4a
SvetlanaPopenko <spopenko02@gmail.com>
2023-02-18 21:48:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,74 +1,81 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Statistics from './Statistics';
 import PropTypes from 'prop-types';
 import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
 import Notification from './Notification';
 
-export class App extends Component {
-  static defaultProps = {
-    state: PropTypes.shape({
-      good: PropTypes.number.isRequired,
-      neutral: PropTypes.number.isRequired,
-      bad: PropTypes.number.isRequired,
-    }).isRequired,
+export const App = () => {
+  const options = ['good', 'neutral', 'bad'];
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
+    }
+  };
+  const countTotalFeedback = () => {
+    const total = good + neutral + bad;
+    return total;
   };
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good * 100) / countTotalFeedback() || 0);
   };
 
-  onLeaveFeedback = event => {
-    this.setState(prevState => ({ [event]: prevState[event] + 1 }));
-  };
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#eeedf8',
+        backgroundColor: '#2c9af4',
+        paddingTop: '20px',
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={onLeaveFeedback}
+        ></FeedbackOptions>
+      </Section>
 
-  countTotalFeedback = () =>
-    Object.values(this.state).reduce((acc, value) => (acc += value), 0);
-
-  countPositiveFeedbackPercentage = ({ good } = this.state) => {
-    return Math.round((good * 100) / this.countTotalFeedback() || 0);
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'flex-start',
-          flexDirection: 'column',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#eeedf8',
-          backgroundColor: '#2c9af4',
-          paddingTop: '20px',
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedback}
-          ></FeedbackOptions>
+      {!countTotalFeedback() ? (
+        <Notification message="There is no feedback"></Notification>
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          ></Statistics>
         </Section>
-        {!this.countTotalFeedback() ? (
-          <Notification message="There is no feedback"></Notification>
-        ) : (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            ></Statistics>
-          </Section>
-        )}
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
+
+App.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+};
+
